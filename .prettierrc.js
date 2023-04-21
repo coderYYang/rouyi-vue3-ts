8ab8d9efc5b1c1ddca8e4bbd8fe33b5832f7a32e
yy <691335336@qq.com>
2023-04-21 17:05:50 +0800

    [ADD] init project

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: yuchao
+ * @Date: 2022-08-09 09:27:08
+ * @LastEditors: yy 691335336@qq.com
+ * @LastEditTime: 2023-04-21 14:23:43
+ * @Description: 格式化文件配置
+ */
+module.exports = {
+  printWidth: 120, // 一行最多 120 字符
+  tabWidth: 2, // 使用 2 个空格缩进
+  tabs: true, // 使用 tab 缩进
+  semi: false, // 行尾需要有分号
+  singleQuote: true, // 使用单引号
+  trailingComma: 'none', // 末尾不需要逗号
+  bracketSpacing: true, // 大括号内的首尾需要空格
+  singleQuote: true, //字符串是否使用单引号，默认为false，使用双引号
+  trailingComma: 'none', // 结尾处不加逗号
+  htmlWhitespaceSensitivity: 'ignore' // 忽略'>'下落问题
+}

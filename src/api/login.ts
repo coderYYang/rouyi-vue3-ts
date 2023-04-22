811ae021b07186c9832bbd2f72035dc7264b6965
yy <691335336@qq.com>
2023-04-22 18:41:52 +0800

    [IMP] 接口地址修改，eslint规则修改

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,59 +1,71 @@
-import request from '@/utils/request';
+import request from '@/utils/request'
+
+interface LoginParams {
+  userName: string
+  password: string
+}
+// 大脑登陆
+export function ClientLogin(params: LoginParams) {
+  return request({
+    url: '/system/SSO/clientLogin',
+    params
+  })
+}
 
 // 登录方法
 export function login(username: string, password: string, code: string, uuid: string) {
-    const data = {
-        username,
-        password,
-        code,
-        uuid,
-    };
-    return request({
-        url: '/login',
-        headers: {
-            isToken: false,
-        },
-        method: 'post',
-        data: data,
-    });
+  const data = {
+    username,
+    password,
+    code,
+    uuid
+  }
+  return request({
+    url: '/login',
+    headers: {
+      isToken: false
+    },
+    method: 'post',
+    data: data
+  })
 }
 
 // 注册方法
 export function register(data: any) {
-    return request({
-        url: '/register',
-        headers: {
-            isToken: false,
-        },
-        method: 'post',
-        data: data,
-    });
+  return request({
+    url: '/register',
+    headers: {
+      isToken: false
+    },
+    method: 'post',
+    data: data
+  })
 }
 
 // 获取用户详细信息
 export function getInfo() {
-    return request({
-        url: '/getInfo',
-        method: 'get',
-    });
+  return request({
+    url: '/system/getInfo',
+    method: 'get'
+  })
 }
 
 // 退出方法
 export function logout() {
-    return request({
-        url: '/logout',
-        method: 'post',
-    });
+  return request({
+    url: '/logout',
+    method: 'post'
+  })
 }
 
 // 获取验证码
 export function getCodeImg() {
-    return request({
-        url: '/captchaImage',
-        headers: {
-            isToken: false,
-        },
-        method: 'get',
-        timeout: 20000,
-    });
+  return request({
+    url: '/system/captchaImage',
+    headers: {
+      isToken: false
+    },
+    method: 'get',
+    timeout: 20000
+  })
 }
